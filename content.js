0c0927855abe45c808eca1c6890b73453f7de53f
Start Coding Student 1 <session1@START-Coding-MBP-C02PP73BG8WN.local>
2015-09-29 14:59:42 -0700

    Added a loop.

--- content.js
+++ content.js
@@ -1,3 +1,45 @@
 /**
  * Created by session1 on 9/28/15.
  */
+var defeat = true;
+var youHit = Math.floor(Math.random() * 1);
+var damageThisRound = Math.floor(Math.random()*3 + 1);
+var totalDamage = 0;
+while (defeat) {
+    defeat = false;
+}
+var defeat = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random()*4 + 1);
+var totalDamage = 0;
+while (defeat) {
+    if (youHit = 1) {
+        console.log("You did it! You defeated the Keeper of Shadows!");
+    }
+    else {
+        console.log("The Keeper of Shadows defeated you.");
+    }
+    defeat = false;
+}
+
+var defeat = true;
+var youHit = Math.floor(Math.random() * 3);
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+var totalDamage = totalDamage + damageThisRound;
+while (defeat) {
+    if (youHit = 2) {
+        console.log("You did it! You defeated the Keeper of Shadows!");
+        if (totalDamage >= 5) {
+            console.log("Your partner, Robyn, defeated the Keeper of Shadows.");
+            defeat = false;
+        }
+        else {
+            youHit = Math.floor(Math.random() * 3);
+        }
+    }
+    else {
+        console.log("The Keeper of Shadows defeated you.");
+    }
+    defeat = false;
+}
+
